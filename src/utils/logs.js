f6b53369762f4e807911da13fe19d765c062c034
Dutched420 <72705964+Dutched420@users.noreply.github.com>
2023-12-20 23:38:16 +0100

    Add files via upload

--- src/utils/logs.js
+++ src/utils/logs.js
@@ -3,7 +3,6 @@ import chalk from "chalk";
 import yaml from "js-yaml";
 // I don't recommend using the "minimal-discord-webhook-node" package, gave me schizophrenia
 import { Webhook, MessageBuilder } from "discord-webhook-node";
-import { ftopData } from "../minecraft/createBot.js";
 
 let config = yaml.load(fs.readFileSync(`${process.cwd()}/config.yaml`, "utf8"));
 
@@ -20,7 +19,7 @@ export function startMessage(name, version) {
         chalk.white(" ────────────────────────────────────────────────────────────\n"),
         chalk.white(`  [${chalk.greenBright("+")}] ${name} ${chalk.magentaBright(version)}\n`),
         chalk.white(`  [${chalk.greenBright("+")}] Discord: ${chalk.magentaBright(`https://discord.gg/KhHSVVARuC`)}\n`),
-        chalk.white(`  [${chalk.redBright("*")}] Pika-Network version\n`),
+        chalk.white(`  [${chalk.redBright("*")}] This is a free program\n`),
         chalk.white(" ────────────────────────────────────────────────────────────")
     );
 };
@@ -30,7 +29,7 @@ export function botLoggedOn(bot) {
 };
 
 export function botWarped(bot, name, version) {
-    console.log(`  [${chalk.magentaBright("Minecraft")}]: ${bot.username} warped to: ${(config.minecraft.joinCommand).replace("/", "").replace("server ", "")} `);
+    console.log(`  [${chalk.magentaBright("Minecraft")}]: ${bot.username} warped to: ${(config.minecraft.joinCommand).replace("/", "")} `);
     const embed = new MessageBuilder()
         .setAuthor(`${name}`, config.discord.webhookUrl, 'https://discord.com/invite/KhHSVVARuC')
         .setDescription(`**${bot.username}** is online\nWhitelisted **${config.whitelist.length}** IGN(s)\nThere are **${Object.keys(bot.players).length}** players online\nServer Jar: **${bot.game.serverBrand}**`)
@@ -73,42 +72,21 @@ export function vanishedExploitPatchedWebhook() {
     createHook(config.discord.vanishedLogs).send(embed);
 };
 
-export function ftopWebhook() {
-    let factionNames = "";
-    let factionValues = "";
-
-    ftopData.map(faction => {
-        factionNames += `**${faction.place}**. ${faction.name}\n`;
-        factionValues += `${faction.value.replace("§8", "")}\n`;
-    });
-
+export function ftopWebhook(ftop) {
     const embed = new MessageBuilder()
         .setAuthor("Factions Top", logo)
         .setColor("#" + config.discord.webhookColor)
         .setFooter(config.minecraft.serverIP)
-        .addField("Faction", factionNames, true)
-        .addField("Value", factionValues, true )
+        .setDescription(ftop)
         .setTimestamp();
     createHook(config.discord.ftop).send(embed);
 };
 
 export function flistWebhook(flist) {
-    let factionNames = "";
-    let factionOnline = "";
-    let factionClaims = "";
-
-    flist.map(faction => {
-        factionNames += faction.name;
-        factionOnline += faction.online;
-        factionClaims += faction.claims;
-    });
-
     const embed = new MessageBuilder()
         .setAuthor("Factions List", logo)
         .setColor("#" + config.discord.webhookColor)
-        .addField("Faction", factionNames, true)
-        .addField("Online", factionOnline, true)
-        .addField("Claims", factionClaims, true)
+        .setDescription(flist)
         .setFooter(config.minecraft.serverIP)
         .setTimestamp();
     createHook(config.discord.flist).send(embed);
